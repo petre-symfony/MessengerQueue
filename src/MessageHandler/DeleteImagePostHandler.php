1d10d8204fad5c552e969c2e5e1375b210574df4
petrero <petrero@gmail.com>
2019-08-21 18:03:32 +0300

    Dispatching Embedded
    All we need to do now is dispatch the new command. And... technically we could do this in two different places. First, you might be thinking that, in ImagePostController, we could dispatch two different commands right here.
    
    But... I don't love that. The controller is already saying DeleteImagePost. It shouldn't need to issue any other commands. If we choose to break that logic down into smaller pieces, that's up to the handler. In other words, we're going to dispatch this new command from within the command handler. Inception!
    
    Instead of calling $this->photoManager->deleteImage() directly, change the type-hint on that argument to autowire MessageBusInterface $messageBus. Update the code in the constructor... and the property name.
    
    Now, easy: remove the old code and start with: $filename = $imagePost->getFilename(). Then, let's delete it from the database and, at the bottom, $this->messageBus->dispatch(new DeletePhotoFile($filename)).
    
    And... this should... just work: everything is still being handled synchronously.
    
    Let's try it next, think a bit about what happens if part of a handler fails, and make half of the delete process async.

--- src/MessageHandler/DeleteImagePostHandler.php
+++ src/MessageHandler/DeleteImagePostHandler.php
@@ -5,33 +5,38 @@ namespace App\MessageHandler;
 
 
 use App\Message\DeleteImagePost;
+use App\Message\DeletePhotoFile;
 use App\Photo\PhotoFileManager;
 use Doctrine\ORM\EntityManagerInterface;
+use http\Message;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
 
 class DeleteImagePostHandler implements MessageHandlerInterface {
 	/**
-	 * @var PhotoFileManager
+	 * @var MessageBusInterface
 	 */
-	private $photoManager;
+	private $messageBus;
 	/**
 	 * @var EntityManagerInterface
 	 */
 	private $entityManager;
 
 	public function __construct(
-		PhotoFileManager $photoManager,
+		MessageBusInterface $messageBus,
 		EntityManagerInterface $entityManager
 	) {
-		$this->photoManager = $photoManager;
+		$this->messageBus = $messageBus;
 		$this->entityManager = $entityManager;
 	}
 
 	public function __invoke(DeleteImagePost $deleteImagePost) {
 		$imagePost = $deleteImagePost->getImagePost();
-		$this->photoManager->deleteImage($imagePost->getFilename());
+		$filename = $imagePost->getFilename();
 
 		$this->entityManager->remove($imagePost);
 		$this->entityManager->flush();
+
+		$this->messageBus->dispatch(new DeletePhotoFile($filename));
 	}
 }
